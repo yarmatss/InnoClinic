41dab6efa42600f58a527dc3753a6af9c8c64ad6
Stanislau Yarmats <stas.yarmots@gmail.com>
2026-03-26 14:10:29 +0100

    style: add blank lines for readability

--- InnoClinic/Profiles/Profiles.DAL/Interfaces/IBaseRepository.cs
+++ InnoClinic/Profiles/Profiles.DAL/Interfaces/IBaseRepository.cs
@@ -8,16 +8,22 @@ public interface IBaseRepository<T> where T : BaseEntity
     Task<IReadOnlyList<T>> GetAllAsync(
         CancellationToken ct, 
         bool trackChanges = false);
+
     Task<IReadOnlyList<T>> GetByConditionAsync(
         Expression<Func<T, bool>> expression, 
         CancellationToken ct, 
         bool trackChanges = false);
+    
     Task<T?> GetByIdAsync(
         Guid id, 
         CancellationToken ct,
         bool trackChanges = false);
+
     void MarkAdd(T entity);
+    
     void MarkUpdate(T entity);
+    
     void MarkDelete(T entity);
+    
     Task SaveChangesAsync(CancellationToken ct);
 }
